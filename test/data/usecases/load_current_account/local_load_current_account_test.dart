469650140e6cbd16e500721d351ce77ddcee5bc8
Eyder Rios <eyder.rios@gmail.com>
2022-06-29 21:52:47 -0300

    Ensure LocalLoadCurrentAccount calls FetchSecureFetchStorage with correct params

---
+++ test/data/usecases/load_current_account/local_load_current_account_test.dart
@@ -0,0 +1,37 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+class LocalLoadCurrentAccount {
+  final FetchSecureCacheStorage fetchSecureCacheStorage;
+
+  LocalLoadCurrentAccount({required this.fetchSecureCacheStorage});
+
+  Future<void> load() {
+    return fetchSecureCacheStorage.fetchSecure('token');
+  }
+}
+
+abstract class FetchSecureCacheStorage {
+  Future<void> fetchSecure(String key);
+}
+
+class FetchSecureCacheStorageSpy extends Mock
+    implements FetchSecureCacheStorage {
+  void mockFecthSecure(String? key) {
+    when(() => fetchSecure(key ?? any())).thenAnswer((_) => Future(() {}));
+  }
+}
+
+void main() {
+  test('Should call FetchSecureCacheStorage with correct parameter', () async {
+    final fetchSecureCacheStorage = FetchSecureCacheStorageSpy();
+    final sut = LocalLoadCurrentAccount(
+        fetchSecureCacheStorage: fetchSecureCacheStorage);
+
+    fetchSecureCacheStorage.mockFecthSecure(null);
+
+    await sut.load();
+
+    verify(() => fetchSecureCacheStorage.fetchSecure('token')).called(1);
+  });
+}
