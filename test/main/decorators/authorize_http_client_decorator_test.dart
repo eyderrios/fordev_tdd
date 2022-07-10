d6be525ec1d589bec6ccd700fd05132d90df54b5
Eyder Rios <eyder.rios@gmail.com>
2022-07-09 21:15:07 -0300

    Add new header if headers is null

--- test/main/decorators/authorize_http_client_decorator_test.dart
+++ test/main/decorators/authorize_http_client_decorator_test.dart
@@ -1,57 +1,106 @@
 import 'package:faker/faker.dart';
-import 'package:fordev_tdd/main/factories/http/http.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
 import 'package:fordev_tdd/data/cache/cache.dart';
 import 'package:fordev_tdd/data/http/http.dart';
 
-class AuthorizeHttpClientDecorator {
+class AuthorizeHttpClientDecorator<ResponseType> {
   final FetchSecureCacheStorage fetchSecureCacheStorage;
+  final HttpClient<ResponseType> decoratee;
 
-  AuthorizeHttpClientDecorator({required this.fetchSecureCacheStorage});
+  AuthorizeHttpClientDecorator({
+    required this.fetchSecureCacheStorage,
+    required this.decoratee,
+  });
 
-  Future<void> request({
+  Future<ResponseType?> request({
     required String url,
     required String method,
     HttpClientBody? body,
     HttpClientHeaders? headers,
   }) async {
-    await fetchSecureCacheStorage.fetchSecure('token');
+    final token = await fetchSecureCacheStorage.fetchSecure('token');
+    HttpClientHeaders authHeaders = {'x-access-token': token ?? ''};
+    final response = await decoratee.request(
+      url: url,
+      method: method,
+      body: body,
+      headers: authHeaders,
+    );
+    return response;
   }
 }
 
 class FetchSecureCacheStorageSpy extends Mock
     implements FetchSecureCacheStorage {
-  void mockFetchSecure() {
-    when(() => fetchSecure(any())).thenAnswer((_) async => 'some_key_value');
+  void mockFetchSecure(String token) {
+    when(() => fetchSecure(any())).thenAnswer((_) async => token);
+  }
+}
+
+class HttpClientSpy<ResponseType> extends Mock
+    implements HttpClient<ResponseType> {
+  void mockRequest(ResponseType response) {
+    when(() => request(
+          url: any(named: 'url'),
+          method: any(named: 'method'),
+          body: any(named: 'body'),
+          headers: any(named: 'headers'),
+        )).thenAnswer((_) async => response);
   }
 }
 
 void main() {
-  const tokenKey = 'token';
-  late AuthorizeHttpClientDecorator sut;
-  late FetchSecureCacheStorageSpy fetchSecureCacheStorage;
+  late AuthorizeHttpClientDecorator<HttpClientBody> sut;
+  late HttpClientSpy<HttpClientBody> client;
+  late FetchSecureCacheStorageSpy cache;
   late String url;
   late String method;
   late HttpClientBody body;
+  late HttpClientHeaders tokenHeader;
+  late String tokenValue;
+  const tokenKey = 'token';
 
   setUp(() {
-    fetchSecureCacheStorage = FetchSecureCacheStorageSpy();
+    cache = FetchSecureCacheStorageSpy();
+    client = HttpClientSpy();
     sut = AuthorizeHttpClientDecorator(
-      fetchSecureCacheStorage: fetchSecureCacheStorage,
+      fetchSecureCacheStorage: cache,
+      decoratee: client,
     );
     url = faker.internet.httpUrl();
     method = faker.randomGenerator.string(10);
     body = {'some_key': 'some_value'};
+    tokenValue = faker.guid.guid();
+    tokenHeader = {'x-access-token': tokenValue};
   });
 
   test('Should call FetchSecureCacheStorage with correct key', () {
     // Arrange
-    fetchSecureCacheStorage.mockFetchSecure();
+    cache.mockFetchSecure(tokenValue);
+    client.mockRequest({});
     // Act
     sut.request(url: url, method: method, body: body);
     // Assert
-    verify(() => fetchSecureCacheStorage.fetchSecure(tokenKey)).called(1);
+    verify(() => cache.fetchSecure(tokenKey)).called(1);
+  });
+
+  test('Should call decoratee with access token on header', () {
+    //
+    // DECORATEE: the class that is being decorated
+    //
+    // Arrange
+    cache.mockFetchSecure(tokenValue);
+    client.mockRequest({});
+    // Act
+    sut.request(url: url, method: method, body: body);
+    // Assert
+    verify(() => client.request(
+          url: url,
+          method: method,
+          body: body,
+          headers: tokenHeader,
+        )).called(1);
   });
 }
